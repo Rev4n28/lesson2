f0a7d31561e1371653ef74e24b0d92ec81866a0b
Rev4n28 <pavlik.kolev@mail.ru>
2024-12-15 16:00:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// условные операторы (if, elif, else)
+// let num = 30
+// if (num < 20) {
+//     let a = num + 1
+//     console.log("a", a)
+// } else {
+//     console.log("число больше 20")
+// }
+// Условные операторы
+// let age = 3;
+// let num = prompt("угадай возраст от 1 до 10");
+// if(num > 3) alert("Число меньше")
+
+// // Тернальные операторы
+// let a = num > age ? alert("Число меньше") : alert("Число больше");
+
+// switch(имя переменной ) {
+//     case вариант_1:
+
+//     console.log('случай_1')
+//     break
+//     case вариант_2
+//     console.log('случай_2')
+
+//     default:
+//     console.log('случай по умолчанию')
+//     break
+// }
+
+let num = prompt('Напиши в поле друг')
+if (num == "друг") {
+    alert('Привет, друг!')
+} else {
+    alert("Похоже, вы не мой друг")
+}
